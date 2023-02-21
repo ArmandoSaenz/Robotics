d89fb4d6d5d014e726d43f6d773b750e071ed8f5
Usuario <Usuario@DESKTOP-NFC2PNM>
2023-02-21 13:36:25 -0600

    Agregar archivos de proyecto.

---
+++ Mathematics/Vectors/Operations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathematics.Vectors
+{
+    public class Operations
+    {
+        static public float PointProduct(float[] x, float[] y)
+        {
+            if (x.Length != y.Length)
+            {
+                string msg = "The vectors dimension don't match";
+                throw new Exception(msg);
+            }
+            float product = 0;
+            int maxIndex = x.Length;
+            for (int i = 0; i < maxIndex; ++i)
+            {
+                product += x[i] * y[i];
+            }
+            return product;
+        }
+    }
+}
---
+++ Tester/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mathematics.Vectors;
+
+namespace Tester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Probador para producto punto");
+            float[] x, y;
+            string data;
+            string[] elements;
+            float result;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Ingresa el primer vector separando los elementos con una coma");
+                    data = Console.ReadLine();
+                    elements = data.Split(',');
+                    x = Array.ConvertAll(elements, new Converter<string, float>(float.Parse));
+                    Console.WriteLine("Ingresa el segundo vector separando los elementos con una coma");
+                    data = Console.ReadLine();
+                    elements = data.Split(',');
+                    y = Array.ConvertAll(elements, new Converter<string, float>(float.Parse));
+                    result = Operations.PointProduct(x, y);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.WriteLine($"El producto punto es {result}" );
+            Console.ReadKey();
+        }
+    }
+}
